3f567f09cfcd9e37970961da7eecb4538783f088
Sion GENDERS <sion@arcplex.fr>
2021-03-30 14:26:17 +0200

    Updated to show in logs the conn string used

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ const connectionString = `${config.mongodb.connStr}/${config.mongodb.database}`;
 
 if(process.env.MONGODB_URI)
   connectionString = process.env.MONGODB_URI;
-  
+
+console.log("Running with connection string : ", connectionString);
+
 mongoose.connect(
   connectionString,
   {
